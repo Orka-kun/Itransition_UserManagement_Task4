4ce5d8953d0ce1cd486d1752a4f68915cc26780d
Orka Dasgupta <149400295+Orka-kun@users.noreply.github.com>
2025-03-27 01:23:17 +0600

    Update Login.jsx

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -28,7 +28,7 @@ const Login = () => {
     }
 
     try {
-      const res = await axios.post(`${process.env.REACT_APP_API_URL}/login`, { email, password });
+      const res = await axios.post(process.env.REACT_APP_API_URL + '/login', { email, password });
       console.log('Login response:', res.data);
       localStorage.setItem('token', res.data.token);
       localStorage.setItem('username', res.data.username);
